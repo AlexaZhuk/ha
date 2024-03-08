20d24d0603f9459e0231c8838b9d13e139934ac7
Alex <zhukaleksazhuk@gmail.com>
2024-03-08 22:39:41 +0300

    answer

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, kitty!");
+			Console.WriteLine("Hello, hihi!");
 			int i = 0;
-		
+			Console.WriteLine("who is my cat?");
 		}
 	}
 }
