e5ef8384fe83245cc5309a9d99ec8922f461e06d
Alex <zhukaleksazhuk@gmail.com>
2024-03-08 22:31:46 +0300

    рфрф

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, kitty!");
+			int i = 0;
+			if (i == 5)
+			{
+				Console.Write("Hallo");
+			}
+			else
+			{
+				Console.WriteLine("aaa");
+
+			}
 		}
 	}
 }
